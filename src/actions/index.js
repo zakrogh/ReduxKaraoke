7903538c3d35b7d07b46de1d1b55f7e64ae9069f
Student <example@example.com>
2019-12-11 16:46:54 -0800

    dont know what I did

--- src/actions/index.js
+++ src/actions/index.js
@@ -21,18 +21,36 @@ export const requestSong = (title, localSongId) => ({
   title,
   songId: localSongId
 });
+
 /*eslint-disable*/
 export function fetchSongId(title) {
   return function (dispatch) {
     const localSongId = v4();
     dispatch(requestSong(title, localSongId));
-    title = title.replace(" ", "_");
+    title = title.replace(' ', '_');
     return fetch(`http://api.musixmatch.com/ws/1.1/track.search?&q_track=` + title + `&page_size=1&s_track_rating=desc&apikey=${process.env.API_KEY}`).then(
+
       response => response.json(),
       error => console.log('An error occurred.', error)
     ).then(function(json) {
-      console.log('CHECK OUT THIS SWEET API RESPONSE:', json);
+      if (json.message.body.track_list.length > 0) {
+        const musicMatchId = json.message.body.track_list[0].track.track_id;
+        const artist = json.message.body.track_list[0].track.artist_name;
+        const title = json.message.body.track_list[0].track.track_name;
+        fetchLyrics(title, artist, musicMatchId, localSongId, dispatch);
+      } else {
+        console.log('We couldn\'t locate a song under that ID!');
+      }
     });
   };
+}
+
+export function fetchLyrics(title, artist, musicMatchId, localSongId, dispatch) {
+  return fetch(`http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=` + musicMatchId + `&apikey=${process.env.API_KEY}`).then(
+    response => response.json(),
+    error => console.log('An error occurred.', error)
+  ).then(function(json) {
+    console.log('HEY WOW, A SECOND API RESPONSE!', json);
+  });
 }
 /*eslint-enable*/
